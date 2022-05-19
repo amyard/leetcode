c2caad10c9a94ba716445dc894ff391d902e801b
amyard <maksymturenko@gmail.com>
2022-05-20 00:15:24 +0300

    9. palindrome number.

---
+++ Lessons/0009/PalindromeNumber.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode.Lessons._0009;
+
+public class PalindromeNumber
+{
+    public static void Run()
+    {
+        // bool res1 = Execute(121);
+        // Print(res1, true);
+        //
+        // bool res2 = Execute(-121);
+        // Print(res2, false);
+        //
+        // bool res3 = Execute(10);
+        // Print(res3, false);
+        //
+        Execute(12321);
+    }
+
+    public static bool Execute(int n)
+    {
+        int sum = 0;
+        var temp = n;   
+        
+        while(n > 0)      
+        {
+            int r = n % 10;      
+            sum = (sum * 10) + r;      
+            n = n / 10;      
+            //Console.WriteLine($"n: {n}. r: {r}. sum: {sum}.");
+        }
+        
+        return temp == sum;
+    }
+
+    public static void Print(bool current, bool expected)
+    {
+        Console.WriteLine($"Current - {current}. Expected: {expected}");
+        Console.WriteLine("-------------------------");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 ﻿
 using LeetCode.Lessons._0001;
 using LeetCode.Lessons._0003;
+using LeetCode.Lessons._0009;
 
 // Lesson 1.
 // TwoSums.Start();
 
 // Lesson 3.
-LongestSubString.Start();
+// LongestSubString.Start();
+
+// Lesson 9.
+PalindromeNumber.Run();
