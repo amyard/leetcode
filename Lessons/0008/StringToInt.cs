a48b114b1e33cf8a536cbcbd60ff43360c02a5f5
amyard <maksymturenko@gmail.com>
2022-05-20 13:13:50 +0300

    8. worked

--- Lessons/0008/StringToInt.cs
+++ Lessons/0008/StringToInt.cs
@@ -1,4 +1,6 @@
-﻿namespace LeetCode.Lessons._0008;
+﻿using System.Numerics;
+
+namespace LeetCode.Lessons._0008;
 
 public class StringToInt
 {
@@ -11,6 +13,9 @@ public class StringToInt
         Print("words and 987", 0);
         Print("+1", 1);
         Print("+-12", 0);
+        Print("21474836460", 2147483647);
+        Print("9223372036854775808", 2147483647);
+        Print("-91283472332", -2147483648);
     }
 
     public static void Print(string s, int expected)
@@ -30,27 +35,26 @@ public class StringToInt
         {
             if (!Char.IsDigit(span[i]))
             {
-                if (span[i] == '-' || span[i] == '+')
+                if ((span[i] == '-' || span[i] == '+') && i == 0)
                     continue;
                 break;
             }
             
-            if (span[i]-'0' > 0)
+            res = span[i]-'0' == 0 && res == 0 ? res : res * 10 + span[i]-'0';
+
+            if (i > 0 && span[i - 1] == '-')
             {
-                res = res * 10 + span[i]-'0';
-             
-                if (i > 0 && span[i - 1] == '-')
-                    negative = -1;
+                negative = -1;
             }
+            
+            if (res*negative < Int32.MinValue)
+                return Int32.MinValue;
+            
+            if (res*negative > Int32.MaxValue)
+                return Int32.MaxValue;
         }
 
         res *= negative;
-
-        if (res > Int32.MaxValue)
-            return Int32.MaxValue;
-        
-        if (res < Int32.MinValue)
-            return Int32.MinValue;
 
         return (int)res;
     }
