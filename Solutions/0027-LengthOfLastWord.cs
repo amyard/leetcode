14f07cf8e39e30a1718508980a796ef7ad4aeb49
amyard <maksymturenko@gmail.com>
2022-12-29 00:53:17 +0200

    27.LengthOfLastWord . own solution

---
+++ Solutions/0027-LengthOfLastWord.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Solutions;
+
+public class LengthOfLastWord
+{
+    public static void Run()
+    {
+        Print("Hello World", 5);
+        Print("   fly me   to   the moon  ", 4);
+        Print("luffy is still joyboy", 6);
+    }
+
+    public static void Print(string s, int expected)
+    {
+        int res = Execute(s);
+        Console.WriteLine($"current: {res}. expected: {expected}");
+        Console.WriteLine("------------------------");
+    }
+
+    public static int Execute(string s)
+    {
+        if (string.IsNullOrEmpty(s)) return 0;
+
+        return s.Split(" ", StringSplitOptions.RemoveEmptyEntries)[^1].Length;
+    }
+}
