8882c4793c3b2bd3e8b7b06a7b5da577fce70e0d
amyard <maksymturenko@gmail.com>
2022-12-29 02:40:39 +0200

    66. PlusOne - not full

---
+++ Solutions/0066-PlusOne.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.Solutions;
+
+public class PlusOne
+{
+    public static void Run()
+    {
+        Print(new int[] {1,2,3}, new int[] {1,2,4});
+        Print(new int[] {4,3,2,1}, new int[] {4,3,2,2});
+        Print(new int[] {9}, new int[] {1,0});
+        // Print(new int[] {9,9}, new int[] {1,0,0});
+    }
+
+    public static void Print(int[] digits, int[] expected)
+    {
+        int[] res = Execute(digits);
+        Console.WriteLine($"current: {ConvertToString(res)}. expected: {ConvertToString(expected)}");
+        Console.WriteLine("------------------------");
+    }
+
+    public static int[] Execute(int[] digits)
+    {
+        int lastIndexValue = digits[^1] + 1;
+        List<int> lastArray = lastIndexValue
+            .ToString()
+            .Select(x => int.Parse(x.ToString()))
+            .ToList();
+        
+        Array.Resize(ref digits, digits.Length - 1);
+        
+        var asd2 = digits.Concat(lastArray).ToArray();
+        
+        return asd2;
+    }
+
+    private static string ConvertToString(int[] digits) => string.Join(", ", digits);
+}
