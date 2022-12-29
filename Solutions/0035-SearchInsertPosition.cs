b1e3f5ee64bcc817d9302ed72c9fa5cbe8137ce3
amyard <maksymturenko@gmail.com>
2022-12-30 00:32:49 +0200

    35. SearchInsertPosition

---
+++ Solutions/0035-SearchInsertPosition.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+
+namespace LeetCode.Solutions;
+
+public class SearchInsertPosition
+{
+    public static void Run()
+    {
+        Print(new int[] {1,3,5,6}, 5,2);
+        Print(new int[] {1,3,5,6}, 2,1);
+        Print(new int[] {1,3,5,6}, 7,4);
+    }
+
+    public static void Print(int[] nums, int target, int expected)
+    {
+        int res = Execute(nums, target);
+        Console.WriteLine($"current: {res}. expected: {expected}");
+        Console.WriteLine("------------------------");
+    }
+
+    public static int Execute(int[] nums, int target)
+    {
+        int res = Array.IndexOf(nums, target);
+
+        if (res < 0)
+        {
+            //Hashtable hashtable = new Hashtable();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > target)
+                    return i;
+            }
+
+            return nums.Length;
+        }
+        
+        return res;
+    }
+}
