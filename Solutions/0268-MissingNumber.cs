bf5e73c4d66acdf2e1d75194cf017c32a21add6d
amyard <maksymturenko@gmail.com>
2022-12-31 02:14:37 +0200

    268. MissingNumber

---
+++ Solutions/0268-MissingNumber.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode.Solutions;
+
+public class MissingNumber
+{
+    public static void Run()
+    {
+        Print(new int[] {3,0,1}, 2);
+        Print(new int[] {0,1}, 2);
+        Print(new int[] {9,6,4,2,3,5,7,0,1}, 8);
+    }
+
+    public static void Print(int[] nums, int expected)
+    {
+        int res = ExecuteStolen(nums);
+        Console.WriteLine($"current: {res}. expected: {expected}");
+        Console.WriteLine("------------------------");
+    }
+
+    public static int Execute(int[] nums)
+    {
+        int max = nums.Length;
+        if (max != nums.Max()) return max;
+        
+        Array.Sort(nums);
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (i != nums[i])
+                return i;
+        }
+        
+        return 0;
+    }
+
+    public static int ExecuteStolen(int[] nums)
+    {
+        return nums.Length * (nums.Length + 1) / 2 - nums.Sum();
+    }
+}
