b5fc91edc44b9376811e919f41af64d048fad9b6
amyard <maksymturenko@gmail.com>
2023-01-04 19:36:57 +0200

    33 / 81. Search in Rotated Sorted Array

---
+++ Solutions/0033-SearchInRotatedSortedArray.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Solutions;
+
+public class SearchInRotatedSortedArray
+{
+    public static void Run()
+    {
+        Print(new int[]{4,5,6,7,0,1,2}, 0, 4);
+        Print(new int[]{4,5,6,7,0,1,2}, 3, -1);
+        Print(new int[]{1}, 0, -1);
+    }
+
+    public static void Print(int[] nums, int target, int expected)
+    {
+        int res = Execute(nums, target);
+        Console.WriteLine($"current: {res}. expected: {expected}");
+        Console.WriteLine("------------------------");
+    }
+
+    public static int Execute(int[] nums, int target)
+    {
+        return Array.IndexOf(nums, target);
+    }
+}
---
+++ Solutions/0081-SearchInRotatedSortedArray2.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode.Solutions;
+
+public class SearchInRotatedSortedArray2
+{
+    public static void Run()
+    {
+        Print(new int[]{4,5,6,7,0,1,2}, 0, true);
+        Print(new int[]{4,5,6,7,0,1,2}, 3, false);
+        Print(new int[]{1}, 0, false);
+        Print(new int[]{2,2,2,3,2,2,2}, 3, true);
+    }
+
+    public static void Print(int[] nums, int target, bool expected)
+    {
+        bool res = Execute(nums, target);
+        Console.WriteLine($"current: {res}. expected: {expected}");
+        Console.WriteLine("------------------------");
+    }
+
+    public static bool Execute(int[] nums, int target)
+    {
+        if (nums.Length == 0)
+            return false;
+
+        if (nums.Length == 1)
+            return nums[0] == target;
+        
+        int left = 0;
+        int right = nums.Length - 1;
+
+        while (right+1 > left)
+        {
+            if (nums[left] == target || nums[right] == target)
+                return true;
+
+            left++;
+            right--;
+        }
+
+        return false;
+    }
+}
