26a5a90c35c008ec8ab831347ced0923ccb0b380
amyard <maksymturenko@gmail.com>
2023-01-12 01:45:55 +0200

    80. Remove Duplicates from Sorted Array II

---
+++ Solutions/0080-RemoveDuplicatesFromSortedArray2.cs
@@ -0,0 +1,47 @@
+﻿namespace LeetCode.Solutions;
+
+public class RemoveDuplicatesFromSortedArray2
+{
+    public static void Run()
+    {
+        Print(new int[] {1,1,1,2,2,3}, 5);
+        Print(new int[] {0,0,1,1,1,1,2,3,3}, 7);
+    }
+
+    public static void Print(int[] nums, int expected)
+    {
+        int res = Execute(nums);
+        Console.WriteLine($"current: {res}. expected: {expected}");
+        Console.WriteLine("------------------------");
+    }
+
+    public static int Execute(int[] nums)
+    {
+        int leftPointer = 1;
+        int currValAppears = 0;
+
+        for (int n = 1; n < nums.Length; n++)
+        {
+            // only twice appears
+            // different values
+            if (nums[n] == nums[n-1] && currValAppears == 0)
+            {
+                nums[leftPointer] = nums[n];
+                currValAppears++;
+                leftPointer++;
+            }
+            else if (nums[n] != nums[n - 1])
+            {
+                nums[leftPointer] = nums[n];
+                currValAppears = 0;
+                leftPointer++;
+            }
+            
+            //Console.WriteLine($"{n} - {nums[n]}");
+        }
+
+        Console.WriteLine(Helper.ConvertArrayOfDigitsToString(nums));
+        
+        return leftPointer;
+    }
+}
